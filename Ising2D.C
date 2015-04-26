d0c69dcef617b0386a5027c8a1a60b499c91c82b
Nacho <nachosandres@gmail.com>
2015-04-26 17:36:42 +0200

    Initial commit

---
+++ Ising2D.C
@@ -0,0 +1,95 @@
+
+
+#include <iostream>
+#include <random>
+#include <fstream>
+using namespace std;
+
+
+int main(){
+	const int N = 100;
+	int timesteps = 10000;
+
+	double T;
+
+	int spins[N][N];
+	double Hf, Ho;
+	int suma;	
+	double M[timesteps]={};
+
+	int nx, ny;
+
+
+  std::default_random_engine generator;
+  std::uniform_int_distribution<int> distribution_bin(0,1);
+  std::uniform_int_distribution<int> distribution_grid(0,N-1);
+  std::uniform_real_distribution<double> distribution_real(0.0,1.0);
+
+
+	cout <<"Dame el valor de kT" << endl;
+  cin >> T;
+
+	for (int i=0; i<N; i++){
+		for (int j=0; j<N; j++){
+			spins[i][j] = -1 + 2*distribution_bin(generator);
+		}
+	} 
+
+	for (int t=0; t<timesteps; t++){
+
+		for (int i=0; i<(N*N); i++){
+			Ho = 0;
+			Hf = 0;
+			nx = distribution_grid(generator);
+			ny = distribution_grid(generator);
+
+			if (nx > 0){
+				Ho = Ho - spins[nx][ny]*spins[nx-1][ny];
+				Hf = Hf + spins[nx][ny]*spins[nx-1][ny];	
+			}
+			if (nx < N-1){
+				Ho = Ho - spins[nx][ny]*spins[nx+1][ny];
+				Hf = Hf + spins[nx][ny]*spins[nx+1][ny];	
+			}
+			if (ny > 0){
+				Ho = Ho - spins[nx][ny]*spins[nx][ny-1];
+				Hf = Hf + spins[nx][ny]*spins[nx][ny-1];	
+			}
+			if (ny < N-1){
+				Ho = Ho - spins[nx][ny]*spins[nx][ny+1];
+				Hf = Hf + spins[nx][ny]*spins[nx][ny+1];	
+			}
+
+			if (  (Hf<Ho) || ( distribution_real(generator) < exp(-(Hf-Ho)/T) )  ){
+				spins[nx][ny] = -spins[nx][ny];
+			}
+
+		}
+
+		suma=0;
+		for (int i=0; i<N; i++){
+			for (int j=0; j<N; j++){
+				suma += spins[i][j];
+			}
+		}
+		M[t] = double(suma)/N/N;
+	}
+
+
+// open the file (for writing because it is an ofstream, meaning "output file stream")
+	std::ofstream output_spins("spins.txt");
+	std::ofstream output_M("magnet.txt");
+
+	for (int i=0;i<N;i++){
+		for (int j=0;j<N;j++){
+			output_spins << spins[i][j] << " "; // behaves like cout - cout is also a stream
+		}
+		output_spins << "\n";
+	}
+	
+	for (int t=0; t<timesteps; t++){
+		output_M << M[t] << "\n"; // behaves like cout - cout is also a stream
+	}
+
+	return 0;
+}
---
+++ Ising_visual.R
@@ -0,0 +1,16 @@
+spins <- as.matrix(read.table("spins.txt"))
+
+spinup <- which(spins == 1, arr.ind=TRUE)
+spindown <- which(spins == -1, arr.ind=TRUE)
+
+plot(spinup[,1],spinup[,2],
+     xlab="x",ylab="y",main="Spin",
+     pch=15,col=colors()[175],cex=.58)
+points(spindown[,1],spindown[,2],
+       pch=15,col=colors()[225],cex=.58)
+legend(93.5,105,legend=c('+1',' -1'),
+       col=c(colors()[175],colors()[225]), pch=15, cex=.8)
+
+
+M <- as.matrix(read.table("magnet.txt"))
+plot(M,type='l',ylim=c(-1,1))
