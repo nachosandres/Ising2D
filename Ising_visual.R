61fab6232ae1cc6976eec99e5a8d82cb9aaf67e1
Nacho <nachosandres@gmail.com>
2015-06-15 10:00:48 +0200

    Imagenes de evolucion

--- Ising_visual.R
+++ Ising_visual.R
@@ -4,7 +4,7 @@ spinup <- which(spins == 1, arr.ind=TRUE)
 spindown <- which(spins == -1, arr.ind=TRUE)
 
 plot(spinup[,1],spinup[,2],
-     xlab="x",ylab="y",main="Spin, T=2.5",
+     xlab="x",ylab="y",main="Spin, T=3",
      pch=15,col=colors()[175],cex=.58,
      xlim=c(0,100),ylim=c(0,100))
 points(spindown[,1],spindown[,2],
@@ -17,42 +17,63 @@ M <- as.matrix(read.table("magnet.txt"))
 plot(M,type='l',ylim=c(-1,1),
      xlab="t",ylab="M",main="T=2")
 
-M1p <- as.matrix(read.table("magnetT1+.txt"))
+M1p <- as.matrix(read.table("magnet/magnetT1+.txt"))
 plot(M1p,type='l',ylim=c(-1,1),
-     xlab="t",ylab="M",main="T=1")
-M1m <- as.matrix(read.table("magnetT1-.txt"))
-lines(M1m,type='l',ylim=c(-1,1))
-M1d <- as.matrix(read.table("magnetT1doms.txt"))
-lines(M1d,type='l',ylim=c(-1,1))
-M2p <- as.matrix(read.table("magnetT2+.txt"))
+     xlab="t",ylab="<s>",main="T=1",col=2)
+M1m <- as.matrix(read.table("magnet/magnetT1-.txt"))
+lines(M1m,type='l',ylim=c(-1,1),col=3)
+M1d <- as.matrix(read.table("magnet/magnetT1doms.txt"))
+lines(M1d,type='l',ylim=c(-1,1),col=4)
+
+
+M2p <- as.matrix(read.table("magnet/magnetT2+.txt"))
 plot(M2p,type='l',ylim=c(-1,1),
-     xlab="t",ylab="M",main="T=2")
-M2m <- as.matrix(read.table("magnetT2-.txt"))
-lines(M2m,type='l',ylim=c(-1,1))
-M2d <- as.matrix(read.table("magnetT2doms.txt"))
-lines(M2d,type='l',ylim=c(-1,1))
-M215 <- as.matrix(read.table("magnetT2_15.txt"))
-lines(M215,type='l',ylim=c(-1,1),col='orange')
-M22 <- as.matrix(read.table("magnetT2_2.txt"))
+     xlab="t",ylab="<s>",main="T=2",col=2)
+M2m <- as.matrix(read.table("magnet/magnetT2-.txt"))
+lines(M2m,type='l',ylim=c(-1,1),col=3)
+M2d <- as.matrix(read.table("magnet/magnetT2doms.txt"))
+lines(M2d,type='l',ylim=c(-1,1),col=4)
+M215 <- as.matrix(read.table("magnet/magnetT2_15.txt"))
+lines(M215,type='l',ylim=c(-1,1),col='green')
+M22 <- as.matrix(read.table("magnet/magnetT2_2.txt"))
 plot(M22,type='l',ylim=c(-1,1),
-     xlab="t",ylab="M",main="T~Tc")
-M225 <- as.matrix(read.table("magnetT2_25.txt"))
+     xlab="t",ylab="<s>",main="T~2.3",col='blue')
+M225 <- as.matrix(read.table("magnet/magnetT2_25.txt"))
 lines(M225,type='l',ylim=c(-1,1),col='red')
-M23 <- as.matrix(read.table("magnetT2_3.txt"))
-lines(M23,type='l',ylim=c(-1,1),col='blue')
-M235 <- as.matrix(read.table("magnetT2_35.txt"))
-lines(M235,type='l',ylim=c(-1,1),col="green")
-M24 <- as.matrix(read.table("magnetT2_4.txt"))
-lines(M24,type='l',ylim=c(-1,1),col="magenta")
-M25 <- as.matrix(read.table("magnetT2_5.txt"))
-lines(M25,type='l',ylim=c(-1,1),col="purple")
-M3 <- as.matrix(read.table("magnetT3.txt"))
-plot(M3,type='l',ylim=c(-1,1))
+
+
+
+M235 <- as.matrix(read.table("magnet/magnetT2_35.txt"))
+lines(M235,type='l',ylim=c(-1,1),col="orange")
+M24 <- as.matrix(read.table("magnet/magnetT2_4.txt"))
+lines(M24,type='l',ylim=c(-1,1),col="purple")
+
+legend(14000,-0.4,legend=c('T=2.15','T=2.2','T=2.25','T=2.35','T=2.4'),
+       col=c('green','blue','red','orange','purple'), lwd=2)
+
+M25 <- as.matrix(read.table("magnet/magnetT2_5.txt"))
+plot(M25,type='l',xlab="t",ylab="<s>",main="T>2.3",ylim=c(-1,1),col="purple")
+M3 <- as.matrix(read.table("magnet/magnetT3.txt"))
+lines(M3,type='l',ylim=c(-1,1),col="orange")
+legend(15000,0.7,legend=c('T=2.5','T=3'),
+       col=c('purple','orange'), lwd=2)
 M[10000]
 
 Mt <- as.matrix(read.table("meanMvsT.txt"))
 plot(Mt[,1],Mt[,2],ylim=c(-1,1))
 abline(h=0)
+
+M231 <- as.matrix(read.table("magnet/magnetT2_3_1.txt"))
+plot(M231,type='l',ylim=c(-1,1),
+     xlab="t",ylab="<s>",main="T=2.3")
+M232 <- as.matrix(read.table("magnet/magnetT2_3_2.txt"))
+lines(M232,type='l',ylim=c(-1,1),col='red')
+M233 <- as.matrix(read.table("magnet/magnetT2_3_3.txt"))
+lines(M233,type='l',ylim=c(-1,1),col='green')
+M234 <- as.matrix(read.table("magnet/magnetT2_3_4.txt"))
+lines(M234,type='l',ylim=c(-1,1),col='blue')
+abline(h=1)
+abline(h=-1)
 
 library(Hmisc)
 Mmeant <- as.matrix(read.table("Mmean.txt"))
@@ -66,4 +87,6 @@ abline(v=2.3)
 # title(main="Variación interanual")
 # mtext("Ajuste a constante")
 errbar(Mmeant[,1],Mmeant[,2],Mmeant[,2]+Mmeant[,3],Mmeant[,2]-Mmeant[,3],
-       xlab="T",ylab="M",pch=20,cex=.5)
+       xlab="T",ylab="<s>",pch=20,cex=.5,col="black",errbar.col="orange")
+title("Magnetización promedio")
+points(Mmeant[,1],Mmeant[,2],pch=20,cex=.5,col="black")
